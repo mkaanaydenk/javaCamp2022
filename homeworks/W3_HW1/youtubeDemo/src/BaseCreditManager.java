9c5aeb4fe652cb3feac3344b6a2ffc07e1b9d6f5
Mehmet Kaan Aydenk <54694153+evolutiontr@users.noreply.github.com>
2022-10-16 19:46:02 +0300

    Add files via upload

---
+++ homeworks/W3_HW1/youtubeDemo/src/BaseCreditManager.java
@@ -0,0 +1,9 @@
+public abstract class BaseCreditManager implements ICreditManager {
+
+    public abstract void calculate();
+
+    public void save() {
+        System.out.println("Kaydedildi");
+    }
+
+}
